71ba66056c8f6d8750ea37337ec0f1312a879e06
Rodrigo Debian <rosec@protonmail.ch>
2018-11-21 15:50:20 -0600

    creating types

---
+++ inventario.hs
@@ -0,0 +1,31 @@
+module Inventario
+( Producto
+, Proveedor
+, Stock
+) where
+
+import Data.Time.Calendar
+import Data.UUID
+
+data Producto = Producto { productoId :: UUID
+               , folio :: Int
+               , nombre :: String
+               , color :: String
+               , unidad :: String
+               , proveedor :: UUID
+               } deriving (Show)
+
+data Proveedor = Proveedor { proveedorId :: UUID
+              , empresa :: String
+              , contacto :: String
+              , domicilio :: String
+              , telefono :: String
+              , email :: String
+              } deriving (Show)
+
+data Stock = Stock { id :: UUID
+              , producto :: UUID
+              , cantidad :: Double
+              , precio :: Double
+              , fecha :: Day
+              } deriving (Show)
