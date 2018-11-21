ed2bc8a429ad80af1e1cf1204d82b1906a74c989
Rodrigo Debian <rosec@protonmail.ch>
2018-11-21 16:01:51 -0600

    sorting classes

--- inventario.hs
+++ inventario.hs
@@ -9,14 +9,6 @@ import Data.UUID
 
 data MovimientoStock = Entrada | Salida deriving (Show)
 
-data Producto = Producto { productoId :: UUID
-               , folio :: Int
-               , nombre :: String
-               , color :: String
-               , unidad :: String
-               , proveedor :: UUID
-               } deriving (Show)
-
 data Proveedor = Proveedor { proveedorId :: UUID
               , empresa :: String
               , contacto :: String
@@ -24,6 +16,14 @@ data Proveedor = Proveedor { proveedorId :: UUID
               , telefono :: String
               , email :: String
               } deriving (Show)
+              
+data Producto = Producto { productoId :: UUID
+               , folio :: Int
+               , nombre :: String
+               , color :: String
+               , unidad :: String
+               , proveedor :: UUID
+               } deriving (Show)
 
 data Stock = Stock { id :: UUID
               , producto :: UUID
