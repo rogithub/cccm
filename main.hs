bd05b3b9e2d616da3450917ff349924706aa34f2
Rodrigo Debian <rosec@protonmail.ch>
2018-11-23 17:06:44 -0600

    ordering

--- main.hs
+++ main.hs
@@ -1,13 +1,13 @@
 import ConfeccionesColombia.Tipos
 import ConfeccionesColombia.ProveedoresDb
 main = do
-  getAll (\l -> putStrLn $ show l)
   let p = Proveedor { proveedorId = 0,
     empresa = "Liconsa",
     contacto = "Rodrigo",
     domicilio = "Juarez 1000",
     telefono = "4521329604",
     email = "correo.rodrigo@gmail.com",
-    comentarios = "chigo cabron",
+    comentarios = "chido cabron",
     activo = True }
   save p
+  getAll (\l -> putStrLn $ show l)
