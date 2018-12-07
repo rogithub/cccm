014407b214b4da44a420a049f711960031e3888d
rodrigo debian <correo.rodrigo@gmail.com>
2018-12-07 14:42:52 -0600

    cleanning code

--- main.hs
+++ main.hs
@@ -2,22 +2,13 @@ module Main where
 
 import ConfeccionesColombia.Tipos
 import ConfeccionesColombia.ProveedoresDb
-import Control.Monad (msum)
+import Control.Monad              ( msum )
+import Control.Monad.IO.Class     ( liftIO )
 import Data.ByteString.Char8 as C
-import Happstack.Server (Method(GET, POST), dir, method, nullConf, ok, toResponseBS, simpleHTTP)
 import Data.Aeson
-import Control.Monad.IO.Class        ( liftIO )
-
-getProveedor :: Proveedor
-getProveedor = Proveedor { proveedorId = 0,
-  empresa = "Liconsa",
-  contacto = "Rodrigo",
-  domicilio = "Juarez 1000",
-  telefono = "4521329604",
-  email = "correo.rodrigo@gmail.com",
-  comentarios = "chido cabron",
-  activo = True }
-
+import Happstack.Server           (Method(GET, POST),
+                                  dir, method, nullConf, ok,
+                                  toResponseBS, simpleHTTP)
 
 main :: IO ()
 main = simpleHTTP nullConf $ msum
