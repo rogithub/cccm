b3ceb60004ccc7470c33f28239f5a7ec5f11ea9b
rodrigo debian <correo.rodrigo@gmail.com>
2018-12-08 06:14:37 -0600

    organizing actions

--- main.hs
+++ main.hs
@@ -6,14 +6,19 @@ import Control.Monad              ( msum )
 import Control.Monad.IO.Class     ( liftIO )
 import Data.ByteString.Char8 as C
 import Data.Aeson
-import Happstack.Server           (Method(GET, POST),
-                                  dir, method, nullConf, ok,
+import Happstack.Server           (Response, ServerPart, Method(GET, POST),
+                                  dirs, method, nullConf, ok,
                                   toResponseBS, simpleHTTP)
 
 main :: IO ()
-main = simpleHTTP nullConf $ msum
-       [ dir "getAll" $ do
-         method GET
-         all <- liftIO $ getAll
-         ok $ toResponseBS (C.pack "application/json") (encode all)
-       ]
+main = simpleHTTP nullConf $ handlers
+
+listarClientes :: ServerPart Response
+listarClientes = do
+  method GET
+  all <- liftIO $ getAll
+  ok $ toResponseBS (C.pack "application/json") (encode all)
+
+handlers :: ServerPart Response
+handlers =
+  msum [ dirs "clientes/lista" $ listarClientes ]
