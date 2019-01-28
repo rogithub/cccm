6962c214eb8522ad154b693b723c3e3e18b8ce33
rodrigo debian <correo.rodrigo@gmail.com>
2019-01-28 16:34:03 -0600

    compiling right

--- src/Pagination/Page.hs
+++ src/Pagination/Page.hs
@@ -7,21 +7,12 @@ module Pagination.Page
 -- SELECT *, count(*) OVER() as TOTAL_RECORDS FROM public."Productos" WHERE id < 6 AND activo=true ORDER BY id LIMIT 5;
 -- SELECT *, count(*) OVER() as TOTAL_ROWS FROM public."Productos" WHERE activo=true ORDER BY ID OFFSET 15 FETCH NEXT 5 ROWS ONLY;
 
-
-import Data.Aeson
-import GHC.Generics
-
-data Page a = Page { rows :: a[],
+data Page a = Page { rows :: a[]
                , pageSize :: Int
                , more :: Bool
-               } deriving (Generic, Show)
+               }
 
-data Direction = Left | Right deriving (Generic, Show)
+data Direction = Left | Right
 
 type LastId = Int
-data PageQuery = (Direction, LastId) deriving (Generic, Show)
-
-instance ToJSON Page where
- toEncoding = genericToEncoding defaultOptions
-
-instance FromJSON Page
+data PageQuery = PageQuery(Direction, LastId)
