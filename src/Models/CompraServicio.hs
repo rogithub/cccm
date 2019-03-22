c58ecf9d106b9fa0a50ac2fcc46bc540d1fa15bf
rodrigo debian <correo.rodrigo@gmail.com>
2019-03-22 08:39:05 -0600

    tabs fix

--- src/Models/CompraServicio.hs
+++ src/Models/CompraServicio.hs
@@ -7,12 +7,12 @@ import GHC.Generics
 import Data.Time.Calendar
 import Data.UUID
 
-data CompraServicio = CompraServicio { idCompraServicio :: Int
-                                     , guidCompraServicio :: UUID
-                , descripcion :: String
-                , cantidad :: Double
-                , precio :: Double
-
+data CompraServicio =
+  CompraServicio { idCompraServicio :: Int
+                 , guidCompraServicio :: UUID
+                 , descripcion :: String
+                 , cantidad :: Double
+                 , precio :: Double
                } deriving (Generic, Show)
 instance ToJSON CompraServicio where
  toEncoding = genericToEncoding defaultOptions
