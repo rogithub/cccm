a1e6c263b0adc1053ff34b0e0099d324626894d0
rodrigo debian <correo.rodrigo@gmail.com>
2019-04-25 20:01:42 -0500

    functor start

--- src/TableMappings/Types/DbRowClass.hs
+++ src/TableMappings/Types/DbRowClass.hs
@@ -10,3 +10,12 @@ import Database.HDBC
 class DbRow a where
   toType :: [SqlValue] -> a
   fromType :: a -> [SqlValue]
+
+
+data DbRecord a = DbRecord a
+
+
+instance Functor DbRecord where
+  fmap f (DbRecord a) = DbRecord (f a)
+
+
