8748a8e7f70103b376163de4fb9db18e872bf987
rodrigo debian <correo.rodrigo@gmail.com>
2019-05-02 15:43:20 -0500

    adding comprasdb

---
+++ src/TableMappings/ComprasDb.hs
@@ -0,0 +1,78 @@
+module TableMappings.ComprasDb
+(
+  getOne,
+  save,
+  update,
+  delete,
+
+  toType,
+  fromType,
+  selOneCmd,
+  savCmd,
+  updateCmd,
+  deleteCmd
+  
+) where
+
+import Database.HDBC
+import DataAccess.Commands
+import DataAccess.Entities
+import DataAccess.PageResult
+import TableMappings.Types.Compra
+import Data.UUID
+import Data.Time.Calendar
+
+instance ToType Compra where
+  toType r =
+    Compra { idCompra = fromSql (r!!0)::Int,
+             guidCompra = read (fromSql (r!!1)::String),
+             proveedorId = read (fromSql (r!!2)::String),
+             fecha = fromSql (r!!3)::Day,
+             docIdFacturaPdf = read (fromSql (r!!4)::String),
+             docIdFacturaXml = read (fromSql (r!!5)::String),
+             iva = fromSql (r!!6)::Double,
+             activo = fromSql (r!!7)::Bool }
+
+fromType :: Compra -> [SqlValue]
+fromType t =
+  [toSql $ toString (proveedorId t),
+   toSql $ fecha t,
+   toSql $ "", --docIdFacturaPdf t
+   toSql $ "", --docIdFacturaXml t
+   toSql $ iva t,
+   toSql $ activo t,
+   toSql $ toString (guidCompra t),
+   toSql $ idCompra t]
+
+
+selOneCmd :: Int -> Command
+selOneCmd key =
+  Command "SELECT * FROM compras where id = ?" [toSql key]
+
+savCmd :: Compra -> Command
+savCmd p =
+  Command "INSERT INTO compras \
+  \ (proveedorId, fecha, docidfacturapdf, docidfacturaxml, ivaporcien, activo, guid)\
+  \ values (?,?,?,?,?,?,?)" (init $ fromType p)
+
+updateCmd :: Compra -> Command
+updateCmd p =
+  Command "UPDATE compras SET \
+  \ proveedorId=?, fecha=?, docidfacturapdf=?, docidfacturaxml=?, ivaporcien=?, activo=?\
+  \ where guid=? and id=?" (fromType p)
+
+deleteCmd :: Int -> Command
+deleteCmd key =
+  Command "UPDATE compras SET activo=? where id=?" [toSql False, toSql key]
+
+getOne :: Int -> IO (Maybe Compra)
+getOne = selectOne . selOneCmd
+
+save :: (Maybe Compra) -> IO Integer
+save = persist savCmd 
+
+update :: (Maybe Compra) -> IO Integer
+update = persist updateCmd
+
+delete :: Int -> IO Integer
+delete = execNonSelQuery . deleteCmd
